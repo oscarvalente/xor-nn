7a8ab3c3ae90866e25f2288f823b6c7e1a1552ae
Oca <Oca@Oscar-PC.lan>
2016-12-29 00:14:36 +0000

    Transpose matrix with tail recursive functions

--- src/blas.erl
+++ src/blas.erl
@@ -12,6 +12,37 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([count_matrix_columns/1]).
+-export([
+ count_matrix_columns/1,
+ transpose_matrix/1
+]).
 
 count_matrix_columns(Matrix) -> length(lists:nth(1, Matrix)).
+
+transpose_matrix(Matrix) ->
+	attach_row(Matrix, []).
+  
+attach_row([], Result) ->
+  reverse_rows(Result, []);
+
+attach_row(RowList, Result) ->
+  [FirstRow | OtherRows] = RowList,
+  NewResult = make_column(FirstRow, Result, []),
+  attach_row(OtherRows, NewResult).
+                                         
+make_column([], _, New) -> lists:reverse(New);
+  
+make_column(Row, [], Accumulator) ->
+  [RowHead | RowTail] = Row,
+  make_column(RowTail, [], [[RowHead] | Accumulator]);
+  
+make_column(Row, Result, Accumulator) ->
+  [RowHead | RowTail] = Row,
+  [ResultHead | ResultTail] = Result,
+  make_column(RowTail, ResultTail, [[RowHead | ResultHead] | Accumulator]).
+
+reverse_rows([], Result) -> lists:reverse(Result);
+
+reverse_rows(Rows, Result) ->
+  [First | Others] = Rows,
+  reverse_rows(Others, [lists:reverse(First) | Result]).
